fa427155d7d2b9abca57aa7c589489f41046e670
Rohan Gunge <321rohan0011@dbit.in-github>
2022-08-09 23:24:29 +0530

    java assignment

---
+++ salary.java
@@ -0,0 +1,9 @@
+public class salary {
+    public static void main(String[] args) {
+        int empno, basic, empname,pf,hra,da,pt,cca,gross,net,i;
+        for(i=0;i<5;i++){
+            System.out.println("Enter employee name :");
+            import java.util.Scanner;
+        }
+    }
+}
